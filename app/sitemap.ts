81b59ec9408a0985acd83b7ca1ce191e914f34d4
mfaeezshabbir <faizytech1st@gmail.com>
2025-12-20 00:29:05 +0500

    chore: Update base URL in robots and sitemap configuration files.

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = "https://postlin.com"; // Update with your actual domain
+  const baseUrl = "https://postlin.mfaeezshabbir.me"; // Update with your actual domain
 
   return [
     {
